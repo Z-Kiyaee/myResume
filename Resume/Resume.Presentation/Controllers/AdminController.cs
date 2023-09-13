28b30141c450044b80d11a7795b9256aca15437c
Z-Kiyaee <z.k1300z.k@gmail.com>
2023-09-13 13:01:23 +0330

    create the base of resume

---
+++ Resume/Resume.Presentation/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Presentation.Controllers
+{
+	public class AdminController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+        public IActionResult Index(string Email, string Username)
+        {
+            return View();
+        }
+    }
+}
