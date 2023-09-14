c345ee9b082dec43e01f175bf8cd67d5fcc28843
Z-Kiyaee <z.k1300z.k@gmail.com>
2023-09-14 17:15:39 +0330

    create rest of models base

--- Resume/Resume.Presentation/Models/Entities/Experience.cs
+++ Resume/Resume.Presentation/Models/Entities/Experience.cs
@@ -1,6 +1,11 @@
-﻿namespace Resume.Presentation.Models.Entities
+﻿namespace Resume.Presentation.Models.Entities;
+
+public class Experience
 {
-    public class Experience
-    {
-    }
+    public int Id { get; set; }
+    public string ExperienceTitle { get; set;}
+    public string ExperienceDuration { get; set;}
+    public string Description { get; set;}
+    public string CompanyName { get; set;}
+    public string? CompanySite { get; set;}
 }
