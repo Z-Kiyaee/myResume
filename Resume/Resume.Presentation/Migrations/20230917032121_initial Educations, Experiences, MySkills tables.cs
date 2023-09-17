3eece4879760b2afd02d78548dbda4e6c38e7a6a
Z-Kiyaee <z.k1300z.k@gmail.com>
2023-09-17 07:03:57 +0330

    create and connect Educations, Experiences, MySkills tables to Database.

---
+++ Resume/Resume.Presentation/Migrations/20230917032121_initial Educations, Experiences, MySkills tables.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Resume.Presentation.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialEducationsExperiencesMySkillstables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Educations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EducatationTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EducationDuration = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Educations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Experiences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExperienceTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExperienceDuration = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanySite = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Experiences", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MySkills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SkillTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Percentage = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MySkills", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Educations");
+
+            migrationBuilder.DropTable(
+                name: "Experiences");
+
+            migrationBuilder.DropTable(
+                name: "MySkills");
+        }
+    }
+}
---
+++ Resume/Resume.Presentation/Models/ResumeDbContext/ResumeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Resume.Presentation.Models.Entities;
+
+namespace Resume.Presentation.Models.ResumeDbContext;
+
+public class ResumeDbContext: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=DESKTOP-GT304K5\\MYSQLSERVER;Initial Catalog=Resume;Integrated Security=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    public DbSet<Education> Educations { get; set; }
+    public DbSet<Experience> Experiences { get; set; }
+    public DbSet<MySkills> MySkills { get; set; }
+}
