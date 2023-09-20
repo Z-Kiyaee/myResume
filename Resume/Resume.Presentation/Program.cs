9072f8bb0f70416d0cf79d2b7ba494f12e036ba8
Z-Kiyaee <z.k1300z.k@gmail.com>
2023-09-20 14:23:24 +0330

    get list of educations from database

--- Resume/Resume.Presentation/Program.cs
+++ Resume/Resume.Presentation/Program.cs
@@ -1,7 +1,11 @@
+using Resume.Presentation.Models.ResumeDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ResumeDbContext>();
 
 var app = builder.Build();
 
